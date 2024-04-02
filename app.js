823ebd0de6490ec228551446fae335f1f154ad2c
Wehan99 <wehanhimsara@gmail.com>
2024-04-02 12:04:21 +0530

    commit2

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-function combine(input1, input2) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var ages = combine(12, 12);
-console.log(ages);
-var namess = combine('Alex', 'Sam');
-console.log(namess);
+function printResult(num) {
+    console.log("Results = " + num);
+}
+printResult(add(10, 15));
